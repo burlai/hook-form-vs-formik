87e068d079969fbefce830da3b7bef54b175ea31
konstantyn-malczewski <konstantyn.malczewski@gmail.com>
2023-05-11 22:30:50 +0200

    Basic example of Formik + Bootstrap styles

--- src/components/Formik.tsx
+++ src/components/Formik.tsx
@@ -1,31 +1,131 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { FormValues } from "../interfaces";
 
-const Formik: React.FC = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+const initialValues: FormValues = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+  zipCode: "",
+};
+
+const validate = (values: FormValues): Partial<FormValues> => {
+  const errors: Partial<FormValues> = {};
+
+  if (!values.name) {
+    errors.name = "Name is required";
+  }
+
+  if (!values.email) {
+    errors.email = "Email is required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.phoneNumber) {
+    errors.phoneNumber = "Phone number is required";
+  }
+
+  if (!values.zipCode) {
+    errors.zipCode = "ZIP code is required";
+  }
+
+  return errors;
+};
+
+const FormComponent: React.FC = () => {
+  const handleSubmit = (data: FormValues) => {
+    console.log("Formik data:");
+    console.log(data);
+  };
+
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validate={validate}
+      onSubmit={handleSubmit}
+    >
+      {({ errors, touched }) => (
+        <Form className="container mt-4">
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <Field
+              type="text"
+              id="name"
+              name="name"
+              className={`form-control ${
+                errors.name && touched.name ? "is-invalid" : ""
+              }`}
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <Field
+              type="email"
+              id="email"
+              name="email"
+              className={`form-control ${
+                errors.email && touched.email ? "is-invalid" : ""
+              }`}
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phoneNumber" className="form-label">
+              Phone Number
+            </label>
+            <Field
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              className={`form-control ${
+                errors.phoneNumber && touched.phoneNumber ? "is-invalid" : ""
+              }`}
+            />
+            <ErrorMessage
+              name="phoneNumber"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="zipCode" className="form-label">
+              ZIP Code
+            </label>
+            <Field
+              type="text"
+              id="zipCode"
+              name="zipCode"
+              className={`form-control ${
+                errors.zipCode && touched.zipCode ? "is-invalid" : ""
+              }`}
+            />
+            <ErrorMessage
+              name="zipCode"
+              component="div"
+              className="invalid-feedback"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
-export default Formik;
+export default FormComponent;
--- src/components/HookForm.tsx
+++ src/components/HookForm.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
-
-type FormValues = {
-  name: string;
-  email: string;
-  phoneNumber: string;
-  zipCode: string;
-};
+import { FormValues } from "../interfaces";
 
 const FormComponent: React.FC = () => {
   const {
@@ -15,32 +9,35 @@ const FormComponent: React.FC = () => {
     formState: { errors },
   } = useForm<FormValues>();
 
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
+  const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
+    console.log("React Hook Form data:");
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="mb-2">
-        <label htmlFor="name" className="me-2">
+    <form onSubmit={handleSubmit(onSubmit)} className="container mt-4">
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
           type="text"
           id="name"
+          className={`form-control ${errors.name ? "is-invalid" : ""}`}
           {...register("name", { required: "Name is required" })}
         />
         {errors.name && (
-          <span className="invalid-feedback">{errors.name.message}</span>
+          <span className="text-danger">{errors.name.message}</span>
         )}
       </div>
-      <div className="mb-2">
-        <label htmlFor="email" className="me-2">
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
           Email
         </label>
         <input
           type="email"
           id="email"
+          className={`form-control ${errors.email ? "is-invalid" : ""}`}
           {...register("email", {
             required: "Email is required",
             pattern: {
@@ -50,36 +47,38 @@ const FormComponent: React.FC = () => {
           })}
         />
         {errors.email && (
-          <span className="invalid-feedback">{errors.email.message}</span>
+          <span className="text-danger">{errors.email.message}</span>
         )}
       </div>
-      <div className="mb-2">
-        <label htmlFor="phoneNumber" className="me-2">
+      <div className="mb-3">
+        <label htmlFor="phoneNumber" className="form-label">
           Phone Number
         </label>
         <input
           type="tel"
           id="phoneNumber"
+          className={`form-control ${errors.phoneNumber ? "is-invalid" : ""}`}
           {...register("phoneNumber", { required: "Phone number is required" })}
         />
         {errors.phoneNumber && (
-          <span className="invalid-feedback">{errors.phoneNumber.message}</span>
+          <span className="text-danger">{errors.phoneNumber.message}</span>
         )}
       </div>
-      <div className="mb-2">
-        <label htmlFor="zipCode" className="me-2">
+      <div className="mb-3">
+        <label htmlFor="zipCode" className="form-label">
           ZIP Code
         </label>
         <input
           type="text"
           id="zipCode"
+          className={`form-control ${errors.zipCode ? "is-invalid" : ""}`}
           {...register("zipCode", { required: "ZIP code is required" })}
         />
         {errors.zipCode && (
-          <span className="invalid-feedback">{errors.zipCode.message}</span>
+          <span className="text-danger">{errors.zipCode.message}</span>
         )}
       </div>
-      <button className="btn btn-primary" type="submit">
+      <button type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
